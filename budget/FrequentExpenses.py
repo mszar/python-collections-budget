14366288f1f04cf54119b6401b76191b01cf2106
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-03-14 21:01:04 +0100

    added categories and count

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,12 +1,17 @@
 from budget import Expense
+import collections
+import matplotlib.pyplot as plt
 
 
 expenses = Expense.Expenses()
-expenses.read_expenses("data/spending_data.csv")
+expenses.read_expenses("../data/spending_data.csv")
 
 spending_categories = []
-
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
-print(spending_categories)
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(*top5)
+
