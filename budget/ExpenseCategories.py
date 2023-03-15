bcf3d6053b050ec6f02e6d99ec56e87bca3ae264
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-03-15 20:24:05 +0100

    pluralsight

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -40,6 +40,12 @@ def main():
     fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
 
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+
+    ax.pie(divided_expenses_sum, labels= labels, autopct='%1.1f%%')
+
 
 
 if __name__ == "__main__":
